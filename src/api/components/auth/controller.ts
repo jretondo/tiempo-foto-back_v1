34e2f171bb2e2c41d10b0b5d63de35d3fea34270
Javier Retondo <jretondo90@gmail.com>
2024-09-26 16:22:20 -0300

    (add): new route -> list of details sells

--- src/api/components/auth/controller.ts
+++ src/api/components/auth/controller.ts
@@ -7,86 +7,107 @@ import auth from '../../../auth';
 import { Iauth } from 'interfaces/Itables';
 
 export = (injectedStore: typeof StoreType) => {
-    let store = injectedStore;
+  let store = injectedStore;
 
-    const upsert = async (body: Iauth, email: string) => {
-        let newAuth: Iauth;
-        if (body.pass) {
-            newAuth = {
-                usuario: body.usuario,
-                prov: body.prov,
-                pass: await bcrypt.hash(body.pass, 5)
-            };
-            if (body.prov === 1) {
-                const result = await store.update(Tables.AUTH_ADMIN, newAuth, Number(body.id));
-                if (result.affectedRows > 0) {
-                    return await sendPass(body.usuario, body.pass, email, "Nueva contraseña", false, false);
-                } else {
-                    return false;
-                }
-            } else {
-                return await store.update(Tables.AUTH_ADMIN, newAuth, Number(body.id));
-            }
+  const upsert = async (body: Iauth, email: string) => {
+    let newAuth: Iauth;
+    if (body.pass) {
+      newAuth = {
+        usuario: body.usuario,
+        prov: body.prov,
+        pass: await bcrypt.hash(body.pass, 5),
+      };
+      if (body.prov === 1) {
+        const result = await store.update(
+          Tables.AUTH_ADMIN,
+          newAuth,
+          Number(body.id),
+        );
+        if (result.affectedRows > 0) {
+          const userData = await store.query(Tables.ADMIN, { email: email });
+          return await sendPass(
+            body.usuario,
+            body.pass,
+            email,
+            'Nueva contraseña',
+            false,
+            false,
+            `${userData[0].nombre} ${userData[0].apellido}`,
+          );
         } else {
-            const newPass = await passCreator();
-            newAuth = {
-                id: body.id,
-                usuario: body.usuario,
-                prov: 1,
-                pass: await bcrypt.hash(newPass, 5)
-            };
-            const result = await store.insert(Tables.AUTH_ADMIN, newAuth);
-            if (result.affectedRows > 0) {
-                return await sendPass(body.usuario, newPass, email, "Nuevo Usuario", true, false);
-            } else {
-                return false;
-            }
+          return false;
         }
-    }
-
-    const recPass = async (email: string) => {
-        console.log('email', email);
-        const newPass = await passCreator();
+      } else {
+        return await store.update(Tables.AUTH_ADMIN, newAuth, Number(body.id));
+      }
+    } else {
+      const newPass = await passCreator();
+      newAuth = {
+        id: body.id,
+        usuario: body.usuario,
+        prov: 1,
+        pass: await bcrypt.hash(newPass, 5),
+      };
+      const result = await store.insert(Tables.AUTH_ADMIN, newAuth);
+      if (result.affectedRows > 0) {
         const userData = await store.query(Tables.ADMIN, { email: email });
-        console.log('userData', userData);
-        const idUsu = userData[0].id;
-        const usuario = userData[0].usuario;
-        const data: Iauth = {
-            id: idUsu,
-            usuario: usuario,
-            prov: 1,
-            pass: newPass
+        return await sendPass(
+          body.usuario,
+          newPass,
+          email,
+          'Nuevo Usuario',
+          true,
+          false,
+          `${userData[0].nombre} ${userData[0].apellido}`,
+        );
+      } else {
+        return false;
+      }
+    }
+  };
+
+  const recPass = async (email: string) => {
+    console.log('email', email);
+    const newPass = await passCreator();
+    const userData = await store.query(Tables.ADMIN, { email: email });
+    console.log('userData', userData);
+    const idUsu = userData[0].id;
+    const usuario = userData[0].usuario;
+    const data: Iauth = {
+      id: idUsu,
+      usuario: usuario,
+      prov: 1,
+      pass: newPass,
+    };
+
+    return await upsert(data, email);
+  };
+
+  const login = async (username: string, password: string) => {
+    const data3 = await store.query(Tables.AUTH_ADMIN, { usuario: username });
+    const data2 = await store.query(Tables.ADMIN, { usuario: username });
+    const userData = data2[0];
+    const data = {
+      ...data2[0],
+      ...data3[0],
+    };
+    const prov = data.prov;
+    return bcrypt.compare(password, data.pass).then((same) => {
+      if (same) {
+        return {
+          token: auth.sign(JSON.stringify(data)),
+          userData: userData,
+          provisory: prov,
         };
+      } else {
+        throw new Error('información invalida');
+      }
+    });
+  };
 
-        return await upsert(data, email);
-    }
-
-    const login = async (username: string, password: string) => {
-        const data3 = await store.query(Tables.AUTH_ADMIN, { usuario: username })
-        const data2 = await store.query(Tables.ADMIN, { usuario: username })
-        const userData = data2[0]
-        const data = {
-            ...data2[0],
-            ...data3[0]
-        }
-        const prov = data.prov
-        return bcrypt.compare(password, data.pass)
-            .then(same => {
-                if (same) {
-                    return {
-                        token: auth.sign(JSON.stringify(data)),
-                        userData: userData,
-                        provisory: prov
-                    }
-                } else {
-                    throw new Error('información invalida')
-                }
-            })
-    }
-
-    return {
-        upsert,
-        login,
-        recPass
-    }
-}
+  return {
+    upsert,
+    login,
+    recPass,
+  };
+};
--- src/utils/sendEmails/sendPass.ts
+++ src/utils/sendEmails/sendPass.ts
@@ -7,54 +7,61 @@ import Names from '../data/Names.json';
 import { IEmailSendPass } from '../../interfaces/IEmails';
 
 export const sendPass = async (
-    user: string,
-    newPass: string,
-    email: string,
-    subject: string,
-    newUser: boolean,
-    view: boolean
+  user: string,
+  newPass: string,
+  email: string,
+  subject: string,
+  newUser: boolean,
+  view: boolean,
+  name: string,
 ): Promise<any> => {
-    if (view) {
-
-        return new Promise((resolve, reject) => {
-            resolve(ejs.render(path.join("emails", "Templates", "ForgotPass.ejs")));
-        })
+  if (view) {
+    return new Promise((resolve, reject) => {
+      resolve(ejs.render(path.join('emails', 'Templates', 'ForgotPass.ejs')));
+    });
+  } else {
+    let welcome: Array<string>;
+    if (newUser) {
+      welcome = [
+        `Usted es nuevo usuario del sistema de Administración de ${Names[0].productName}`,
+      ];
     } else {
-
-        let welcome: Array<string>;
-        if (newUser) {
-            welcome = [`Usted es nuevo usuario del sistema de Administración de ${Names[0].productName}`];
-        } else {
-            welcome = [`¿Te olvidaste la contraseña? No te preocupes, a continuación te pasamos los nuevos datos de acceso a ${Names[0].productName}`];
-        }
-
-        const datos: IEmailSendPass = {
-            Colors,
-            Links,
-            Names,
-            titlePage: subject,
-            titleHead: "Hola Retondo Javier",
-            parrafosHead: welcome,
-            titleButton: "A continuación le pasamos los datos de ingreso al mismo:",
-            textCall: `Usuario: ${user}`,
-            textCall2: `Contraseña: ${newPass}`,
-            textFoother: `Útilice esta información para poder ingresar al sistema: <br> <a href='${Links[0].linkApp}' target='_blank'>Aplicación de Administración</a>. El sistema le va a pedir una nueva contraseña segura que usted recuerde.`
-        };
-
-        return new Promise((resolve, reject) => {
-            ejs.renderFile(path.join("views", "emails", "Templates", "ForgotPass.ejs"), datos, async (err, data) => {
-                if (err) {
-                    console.error(err);
-                    resolve(false);
-                } else {
-                    try {
-                        resolve(await sendEmail(email, subject, data))
-                    } catch (error) {
-                        console.error(error);
-                        reject(error);
-                    }
-                }
-            })
-        });
+      welcome = [
+        `¿Te olvidaste la contraseña? No te preocupes, a continuación te pasamos los nuevos datos de acceso a ${Names[0].productName}`,
+      ];
     }
-}
+
+    const datos: IEmailSendPass = {
+      Colors,
+      Links,
+      Names,
+      titlePage: subject,
+      titleHead: `Hola ${name}`,
+      parrafosHead: welcome,
+      titleButton: 'A continuación le pasamos los datos de ingreso al mismo:',
+      textCall: `Usuario: ${user}`,
+      textCall2: `Contraseña: ${newPass}`,
+      textFoother: `Útilice esta información para poder ingresar al sistema: <br> <a href='${Links[0].linkApp}' target='_blank'>Aplicación de Administración</a>. El sistema le va a pedir una nueva contraseña segura que usted recuerde.`,
+    };
+
+    return new Promise((resolve, reject) => {
+      ejs.renderFile(
+        path.join('views', 'emails', 'Templates', 'ForgotPass.ejs'),
+        datos,
+        async (err, data) => {
+          if (err) {
+            console.error(err);
+            resolve(false);
+          } else {
+            try {
+              resolve(await sendEmail(email, subject, data));
+            } catch (error) {
+              console.error(error);
+              reject(error);
+            }
+          }
+        },
+      );
+    });
+  }
+};
